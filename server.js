532b62e1e60ef9bcb568cec468e616273d4f7c28
Admin-Backend <adminbackend@mkfirstaid.org.au>
2025-02-19 14:13:01 +1000

    Update server.js

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.post('/create-checkout-session', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 4242;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
 
 app.get("/", (req, res) => {
     res.send("Server is running!");
